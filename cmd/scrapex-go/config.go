57afcb75c8fffec17308906511e821aa4060d107
Ihor Liubymov <ihor.liubymov@ringteam.com>
2021-11-21 19:30:58 +0200

    first

---
+++ cmd/scrapex-go/config.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/jessevdk/go-flags"
+	scrapex "github.com/mastak/scrapex-go"
+	logger "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+// Options : Cli option Flags
+type Options struct {
+	ConfigPath  flags.Filename `short:"c" long:"config_path" default:"../config/scrapex.yml" env:"CONFIG_PATH" description:"Path To configure"`
+	ScrapeTest  bool           `short:"t" long:"test" description:"To test for crawling via a scraper only\n(Without an emitter module)"`
+	ShowVersion bool           `short:"v" long:"version" description:"Show scrapex version and git revision id"`
+}
+
+var options Options
+
+// Build flags
+var (
+	version = ""
+	commit  = ""
+)
+
+func setOptions() (scrapex.Config, error) {
+	config := scrapex.Config{}
+
+	var parser = flags.NewParser(&options, flags.Default)
+	parser.ShortDescription = `scrapex`
+	parser.LongDescription = `Options for scrapex`
+
+	if _, err := parser.Parse(); err != nil {
+		if fe, ok := err.(*flags.Error); ok {
+			if fe.Type == flags.ErrHelp {
+				os.Exit(0)
+			}
+		}
+		return config, fmt.Errorf("The program has been anomaly exited. Exit code = %d", 1)
+	}
+
+	if options.ShowVersion {
+		fmt.Printf("scrapex %s (git commit: %s)\n", version, commit)
+		os.Exit(0)
+	}
+
+	bytes, err := ioutil.ReadFile(string(options.ConfigPath))
+	if err != nil {
+		return config, err
+	}
+
+	err = yaml.Unmarshal(bytes, &config)
+	if err != nil {
+		return config, err
+	}
+
+	err = setLogger(&config)
+	if err != nil {
+		return config, err
+	}
+
+	logger.WithFields(logger.Fields{
+		"config.Global.MaxUsedCores":            config.Global.MaxUsedCores,
+		"config.Global.Interval":                config.Global.Interval,
+		"config.Global.Log.LogLevel":            config.Global.Log.LogLevel,
+		"config.Global.Log.Stdout":              config.Global.Log.Stdout,
+		"config.Global.Log.Format":              config.Global.Log.Format,
+		"config.Global.Log.ForceColors":         config.Global.Log.ForceColors,
+		"config.Global.Log.GoTimeFormat":        config.Global.Log.GoTimeFormat,
+		"config.Global.Log.LogPath":             config.Global.Log.LogPath,
+		"config.Global.Log.MaxSize":             config.Global.Log.MaxSize,
+		"config.Global.Log.MaxAge":              config.Global.Log.MaxAge,
+		"config.Global.Log.MaxBackups":          config.Global.Log.MaxBackups,
+		"config.Global.Log.Compress":            config.Global.Log.Compress,
+		"config.Scraper.VisitDomains":           config.Scraper.VisitDomains,
+		"config.Scraper.AllowedDomains":         config.Scraper.AllowedDomains,
+		"config.Scraper.UserAgent":              config.Scraper.UserAgent,
+		"config.Scraper.MaxDepthToVisit":        config.Scraper.MaxDepthToVisit,
+		"config.Scraper.Async":                  config.Scraper.Async,
+		"config.Scraper.Parallelism":            config.Scraper.Parallelism,
+		"config.Scraper.Delay":                  config.Scraper.Delay,
+		"config.Scraper.RandomDelay":            config.Scraper.RandomDelay,
+		"config.Scraper.ConsumerQueueThreads":   config.Scraper.ConsumerQueueThreads,
+		"config.Scraper.ConsumerQueueMaxSize":   config.Scraper.ConsumerQueueMaxSize,
+		"config.Emitter.Elasticsearch.Hosts":    config.Emitter.Elasticsearch.Hosts,
+		"config.Emitter.Elasticsearch.Username": config.Emitter.Elasticsearch.Username,
+		"config.Emitter.Elasticsearch.Password": config.Emitter.Elasticsearch.Password,
+	}).Info("Show All Configurations")
+
+	return config, nil
+}
---
+++ pkg/scrapex-go/model.go
@@ -0,0 +1,74 @@
+package scrapex
+
+// Config is the options of scrapex.
+type Config struct {
+	Global struct {
+		MaxUsedCores int `yaml:"max_used_cores"`
+		Interval     int `yaml:"interval"`
+		Log          struct {
+			LogLevel     string `yaml:"log_level"`
+			Stdout       bool   `yaml:"stdout"`
+			Format       string `yaml:"format"`
+			ForceColors  bool   `yaml:"force_colors"`
+			GoTimeFormat string `yaml:"go_time_format"`
+			LogPath      string `yaml:"log_path"`
+			MaxSize      int    `yaml:"max_size"`
+			MaxAge       int    `yaml:"max_age"`
+			MaxBackups   int    `yaml:"max_backups"`
+			Compress     bool   `yaml:"compress"`
+		} `yaml:"log"`
+	} `yaml:"global"`
+
+	Scraper struct {
+		VisitDomains         []string `yaml:"visit_domains"`
+		AllowedDomains       []string `yaml:"allowed_domains"`
+		UserAgent            string   `yaml:"user_agent"`
+		MaxDepthToVisit      int      `yaml:"max_depth_to_visit"`
+		Async                bool     `yaml:"async"`
+		Parallelism          int      `yaml:"parallelism"`
+		Delay                int      `yaml:"delay"`
+		RandomDelay          int      `yaml:"random_delay"`
+		ConsumerQueueThreads int      `yaml:"consumer_queue_threads"`
+		ConsumerQueueMaxSize int      `yaml:"consumer_queue_max_size"`
+	} `yaml:"scraper"`
+
+	Emitter struct {
+		Elasticsearch struct {
+			Hosts    []string `yaml:"hosts"`
+			Username string   `yaml:"username"`
+			Password string   `yaml:"password"`
+		}
+	}
+}
+
+// Types of Config.Global.Log.Format
+const (
+	JSON = "json"
+	TEXT = "text"
+)
+
+// Emitter interface to sent or write the data to the targets.
+type Emitter interface {
+	// Connect to the Emitter; connect is only called once when the plugin starts.
+	Connect() error
+	// Close any connections to the Emitter. Close is called once when the output
+	// is shutting down. Close will not be called until all writes have finished,
+	// and Write() will not be called once Close() has been, so locking is not
+	// necessary.
+	Close()
+	// Write takes in group of points to be written to the Emitter.
+	// this is a consumer in a part of a pipeline.
+	Write(<-chan GachiData) error
+}
+
+// GachiData is contents to collect data by scraper.
+type GachiData struct {
+	Timestamp       string
+	VisitHost       string
+	Creator         string
+	Title           string
+	Description     string
+	URL             string
+	ShortCutIconURL string
+	ImageURL        string
+}
